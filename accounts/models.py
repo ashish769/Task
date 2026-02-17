2506e5db5e4b78b7c7306a5ce9a4be2db2ceee57
ashish <ashishrajpoudel28@gmail.com>
2026-02-18 00:10:27 +0545

    create the usermanager class in model

--- accounts/models.py
+++ accounts/models.py
@@ -24,3 +24,22 @@ class User(AbstractUser):
     @property
     def is_reader(self):
         return self.role == self.Role.READER
+
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None, role="READER", **extra_fields):
+        if not username:
+            raise ValueError("Username required")
+
+        user = self.model(username=username, role=role, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('role', 'ADMIN')
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, password, **extra_fields)
